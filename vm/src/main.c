6c2c37c13927ee0a97e44eb74b6650b71af7ea6e
xiongxin <1781189926@qq.com>
2020-12-28 17:46:35 +0800

    init

---
+++ vm/src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
+#include "vm.h"
+
+int main(int argc, const char *argv[])
+{
+    initVM();
+
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
+    disassembleChunk(&chunk, "test chunk demo 1");
+    interpret(&chunk);
+
+    freeVM();
+    freeChunk(&chunk);
+    return 0;
+}
