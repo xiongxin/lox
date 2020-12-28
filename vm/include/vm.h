3bad27e50b35758fb476510d0cd4041508051ffe
熊鑫 <1781189926@qq.com>
2020-12-29 00:11:21 +0800

    update

--- vm/include/vm.h
+++ vm/include/vm.h
@@ -2,11 +2,16 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256;
 
 typedef struct
 {
     Chunk *chunk;
     uint8_t *ip;
+    Value stack[STACK_MAX];
+    Value *stackTop;
 } VM;
 
 typedef enum
--- vm/src/vm.c
+++ vm/src/vm.c
@@ -1,10 +1,19 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "vm.h"
+#include "debug.h"
 
 VM vm;
 
+static void restStack()
+{
+    vm.stackTop = vm.stack;
+}
+
 void initVM()
 {
+    restStack();
 }
 
 void freeVM()
@@ -14,13 +23,25 @@ void freeVM()
 static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
-
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
     for (;;)
     {
+
+#ifdef DEBUG_TRACE_EXECTURE
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+
         uint8_t instruction;
 
         switch (instruction = READ_BYTE())
         {
+        case OP_CONSTANT:
+        {
+            Value constant = READ_CONSTANT();
+            printValue(constant);
+            printf("\n");
+            break;
+        }
         case OP_RETURN:
         {
             return INTERPRET_OK;
@@ -29,6 +50,7 @@ static InterpretResult run()
     }
 
 #undef READ_BYTE
+#undef READ_CONSTANT
 }
 
 InterpretResult interpret(Chunk *chunk)
