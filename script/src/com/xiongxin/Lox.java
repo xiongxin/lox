850244fe7fb17b6751806182920270f22aaea217
xiongxin <1781189926@qq.com>
2021-01-03 23:14:35 +0800

    update

--- script/src/com/xiongxin/Lox.java
+++ script/src/com/xiongxin/Lox.java
@@ -1,11 +1,15 @@
 package com.xiongxin;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Lox {
+
+    private static boolean hadError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -21,5 +25,41 @@ public class Lox {
     private static void runFile(String path) throws IOException {
         var bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code.
+        if (hadError) System.exit(65);
+    }
+
+
+    private static void runPrompt() throws IOException {
+        var input = new InputStreamReader(System.in);
+        var reader = new BufferedReader(input);
+
+        while (true) {
+            System.out.print(">  ");
+            var line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run(String source) {
+        var scanner = new Scanner(source);
+        var tokens  = scanner.scanTokens();
+
+        for (var token : tokens) {
+            System.out.println(token);
+        }
+    }
+    
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+    
+    private static void report(int line, String where,
+                               String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 }
